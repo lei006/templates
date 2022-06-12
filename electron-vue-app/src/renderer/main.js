bd76d33a70ad1a33f06c8db10292f59cc5140ead
lei006 <lei006@qq.com>
2022-06-12 22:52:06 +0800

    vite + electron

--- electron-vue-app/src/renderer/main.js
+++ electron-vue-app/src/renderer/main.js
@@ -6,6 +6,10 @@ import router from './router'
 import store from './store'
 
 
+
+
+
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
